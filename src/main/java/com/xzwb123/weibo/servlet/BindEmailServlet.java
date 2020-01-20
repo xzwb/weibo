d27ca162c4891137475b085888eaabc3b6d328c1
xzwb <1478863479@qq.com>
2020-01-20 18:03:53 +0800

    添加手机号注册时的正则表达式判断

---
+++ src/main/java/com/xzwb123/weibo/servlet/BindEmailServlet.java
@@ -0,0 +1,24 @@
+package com.xzwb123.weibo.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "BindEmailServlet",
+        urlPatterns = {"/bindEmail"}
+)
+public class BindEmailServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        String judge = (String) req.getAttribute("judge");
+
+    }
+}
