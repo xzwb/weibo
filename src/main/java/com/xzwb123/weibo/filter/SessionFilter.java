5be983f9e409f098de35eb5a8860d20cee588d36
xzwb <1478863479@qq.com>
2020-01-28 14:42:54 +0800

    设置字符集过滤器

--- src/main/java/com/xzwb123/weibo/filter/SessionFilter.java
+++ src/main/java/com/xzwb123/weibo/filter/SessionFilter.java
@@ -2,22 +2,14 @@ package com.xzwb123.weibo.filter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * 设置字符集过滤器
- * 作用: 统一字符格式
- */
-@WebFilter("/*")
-public class EncodingFilter extends HttpFilter {
+public class SessionFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        request.setCharacterEncoding("utf-8");
-        response.setContentType("text/html;charset=utf-8");
-        chain.doFilter(request, response);
+
     }
 }
