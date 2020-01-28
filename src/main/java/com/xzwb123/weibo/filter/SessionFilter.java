0114faabb18ee7d45b3073057fe73a4aa5227d89
xzwb <1478863479@qq.com>
2020-01-28 14:46:43 +0800

    创建用户实例

--- src/main/java/com/xzwb123/weibo/filter/SessionFilter.java
+++ src/main/java/com/xzwb123/weibo/filter/SessionFilter.java
@@ -2,14 +2,22 @@ package com.xzwb123.weibo.filter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+/**
+ * 设置用户session失效后的处理
+ */
+@WebFilter("/*.needSession")
 public class SessionFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession hs = request.getSession();
 
+        if ()
     }
 }
---
+++ src/main/java/com/xzwb123/weibo/info/User.java
@@ -0,0 +1,49 @@
+package com.xzwb123.weibo.info;
+
+public class User {
+    private int uid;
+    private String uname;
+    private String email;
+    private String phoneNumber;
+    private String pwd;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
