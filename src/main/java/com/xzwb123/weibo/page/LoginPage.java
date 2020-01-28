6513b2846907a333db69e7ba81daee17b3679487
xzwb <1478863479@qq.com>
2020-01-28 15:48:29 +0800

    登录界面

--- src/main/java/com/xzwb123/weibo/page/LoginPage.java
+++ src/main/java/com/xzwb123/weibo/page/LoginPage.java
@@ -1,10 +1,30 @@
-@javax.servlet.annotation.WebServlet(name = "LoginPage")
-public class LoginPage extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package com.xzwb123.weibo.page;
 
-    }
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+/**
+ * 登录界面
+ */
+@WebServlet(
+        name = "LoginPage",
+        urlPatterns = {"/loginPage"}
+)
+public class LoginPage extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<img src='1.jpg' width=200px>");
+        out.println("<form action='login' method='post'>");
+        out.println("账号: <input type='text' name='name' placeholder='账号/邮箱/手机号' required><br><br>");
+        out.println("密码: <input type='password' name='pwd' required>");
+        out.println("<input type='submit' value='登录'>  <input type='checkbox' name='autoLogin' value='yes'> 三天内自动登录登陆");
+        out.println("</form>");
+        out.print("<hr>");
     }
 }
